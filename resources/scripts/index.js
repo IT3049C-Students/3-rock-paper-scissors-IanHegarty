a1ad1d651133f74fc3a1912064134cc6963c7465
hegartim <hegartim@mail.uc.edu>
2022-05-27 15:42:59 -0400

    Added some of the game controller logic

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,12 +1,12 @@
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = ;
-const startGameButton = ;
-const userName = ;
-const userSelection = ;
-const goButton = ;
-const scoreParagraph = ;
-const gameHistoryParagraph = ;
+const gameScreen = document.getElementById(`game-screen`);
+const startGameButton = document.getElementById(`start-game-button`);
+const userName = document.getElementById(`username`);
+const userSelection = document.getElementById(`user-selection`);
+const goButton = document.getElementById(`go-button`);
+const scoreParagraph = document.getElementById(`score`);
+const gameHistoryParagraph = document.getElementById(`game-history`);
 
 // instantiate the game object from the `RockPaperScissors` class.
 let game;
@@ -16,24 +16,32 @@ gameScreen.classList.add(`d-none`);
 
 // updateScoreTallyUI
 function updateScoreTallyUI(){
-
+  scoreParagraph.innerHTML = game.username + `:` + game.score.user + ` v CPU: ` + game.score.cpu;
 }
 
 // updateGameHistoryUI
 function updateGameHistoryUI(){
-
+  gameHistoryParagraph.innerHTML = ``;
+  for(let i = 0; i < game.gameHistoryLog.length; i++){
+    gameHistoryParagraph.innerHTML += game.gameHistoryLog[i] + "\n";
+  }
 }
 
 // start-game-button EventListener
 startGameButton.addEventListener(`click`, function () {
-  const username = 
+  event.preventDefault();
   game = new RockPaperScissors(userName);
   // Complete
+  welcomeScreen.classList.add(`d-none`);
+  gameScreen.classList.remove(`d-none`);
 });
 
 // go-button EventListener
 goButton.addEventListener(`click`, function () {
-  
+  event.preventDefault();
+  game.play(userSelection.value);
+  updateScoreTallyUI();
+  updateGameHistoryUI();
 });
 
 // If you're doing the extra-credit, uncomment the below: reset-game-button
